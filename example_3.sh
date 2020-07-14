4d67292726c71c6a340005a6bcbdb4a645e714a5
Shang-Fu Chen <shaunfchen@gmail.com>
2020-07-13 21:18:50 -0700

    first commit.

---
+++ example_3.sh
@@ -0,0 +1,8 @@
+echo command line 1; sleep 0.5
+echo command line 2; sleep 0.5
+echo command line 3; sleep 0.5
+echo command line 4; sleep 0.5
+echo command line 5; sleep 0.5
+echo command line 6; sleep 0.5
+echo command line 7; sleep 0.5
+echo command line 8; sleep 0.5
